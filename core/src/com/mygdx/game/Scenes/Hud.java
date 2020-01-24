36511777d229146e88066d8a2630f8d63edf62a9
Ben <benjosephmartin@gmail.com>
2020-01-24 13:55:39 -0600

    Inactive Play Button

--- core/src/com/mygdx/game/Scenes/Hud.java
+++ core/src/com/mygdx/game/Scenes/Hud.java
@@ -1,9 +1,14 @@
 package com.mygdx.game.Scenes;
 
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.mygdx.game.Utils.Constants;
@@ -11,18 +16,32 @@ import com.mygdx.game.Utils.Constants;
 import javax.swing.text.View;
 
 public class Hud {
-    public Stage stage;
+    private Stage stage;
     private Viewport viewport;
     private int worldTimer;
     private float timeCount;
     private int Score;
     Label countDownLabel;
-    public Hud(SpriteBatch sb){
+    private Texture myTexture;
+    private TextureRegion myTextureRegion;
+    private TextureRegionDrawable myTexRegionDrawable;
+    private ImageButton button;
+    public Hud(SpriteBatch sb, Viewport viewport, Stage stage){
         worldTimer = 300;
         timeCount = 0;
         Score = 0;
-        viewport = new FitViewport(Constants.width, Constants.height, new OrthographicCamera());
-        stage = new Stage(viewport, sb);
+        this.stage = stage;
+        myTexture = new Texture("playImage.png");
+        myTextureRegion = new TextureRegion(myTexture);
+        myTexRegionDrawable = new TextureRegionDrawable(myTextureRegion);
+        button = new ImageButton(myTexRegionDrawable); //Set the button up
+        this.viewport = viewport;
+        //stage = new Stage(viewport, sb);
+        stage.addActor(button);
+        Table table = new Table();
+        table.top();
+        table.setFillParent(true);
+        //stage.draw();
 
     }
 
